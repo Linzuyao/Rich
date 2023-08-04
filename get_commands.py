b09bd7cd1d559f15a54dff3a6cc4cf98ec479d37
Linzuyao <2484453211@qq.com>
2023-08-04 11:07:06 +0800

    Make the mian frame for the project

--- get_commands.py
+++ get_commands.py
@@ -1,2 +1,25 @@
 # Created by Linzuyao
 # The module has the functions and the classes and so on to get user commands
+from colorama import Fore
+
+
+class Command:
+    def __init__(self, role=None):
+        self.role = role
+
+    def get_user_input(self):
+        if self.role == "Q":
+            command = input(Fore.GREEN+">>>>>").strip().lower()
+        elif self.role == "A":
+            command = input(Fore.RED+">>>>>").strip().lower()
+        elif self.role == "J":
+            command = input(Fore.BLUE+">>>>>").strip().lower()
+        else:
+            command = input(">>>>>").strip().lower()
+        print(Fore.RESET, end="")
+        return command
+
+
+if __name__ == "__main__":
+    test = Command()
+    print(test.get_user_input())
--- rich.py
+++ rich.py
@@ -1,2 +1,27 @@
 # Creadted by Linzuyao
 # This is the main file of rich project
+from get_commands import Command
+
+if __name__ == "__main__":
+    later_to_decide = Command()
+
+while True:
+    command = later_to_decide.get_user_input()
+    if command == "quit":
+        break
+    elif command == "roll":
+        pass
+    elif command == "sell":
+        pass
+    elif command == "block":
+        pass
+    elif command == "robot":
+        pass
+    elif command == "help":
+        pass
+    elif command == "query":
+        pass
+    elif command == "step":
+        pass
+    else:
+        print("Your input a bad command,please repeat.")
